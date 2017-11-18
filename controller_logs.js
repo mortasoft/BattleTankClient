f6162a1a53b935cae86aca9fbeebaf105f0d5721
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-18 00:57:05 -0600

    added controler_logs js and html to this specific files. Added the post's method of Logs. Thanks God!

---
+++ controller_logs.js
@@ -0,0 +1,13 @@
+angular.module("MyLogs", [])
+    .controller("LogsController", function ($scope, $http) {
+            $scope.logs = [];
+            $scope.newLog = {};
+            $http.get("http://192.168.0.15:5000/api/v1/users/tank/logs")
+                .then(function (datos) {
+                        console.log(datos);
+                        $scope.logs = datos.data;
+                    },
+                    function (error) {
+                        console.log(error);
+                    });
+});
