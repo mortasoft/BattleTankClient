ea9e4e6c1df8521bb93525fc8beede2946c21136
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-18 10:25:20 -0600

    Deleted some files no used.

--- controller_logs.js
+++ controller_logs.js
@@ -2,7 +2,7 @@ angular.module("MyLogs", [])
     .controller("LogsController", function ($scope, $http) {
             $scope.logs = [];
             $scope.newLog = {};
-            $http.get("http://192.168.0.15:5000/api/v1/users/tank/logs")
+            $http.get("http://10.2.2.214:5000/api/v1/users/tank/logs")
                 .then(function (datos) {
                         console.log(datos);
                         $scope.logs = datos.data;
